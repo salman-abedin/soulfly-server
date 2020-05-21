112cfd3c1319b1ba635aec1ca80209a0f19e4a74
Salman Abedin <salman104alvi@gmail.com>
2020-05-21 22:12:53 +0600

    update

--- index.js
+++ index.js
@@ -66,19 +66,19 @@ app.use(express.json()); // JSON String to OBJECT
 const io = socketIO(server);
 io.on('connect', (socket) => {
 
-   Message.find()
-      .sort({ _id: -1 })
-      .limit(10)
-      .exec((err, messages) => {
-         if (err) return console.error(err);
-         console.log(messages);
-         socket.emit('init-messages', messages.reverse());
-      });
+   // Message.find()
+   //    .sort({ _id: -1 })
+   //    .limit(10)
+   //    .exec((err, messages) => {
+   //       if (err) return console.error(err);
+   //       console.log(messages);
+   //       socket.emit('init-messages', messages.reverse());
+   //    });
 
    socket.on('join', (name) => {
       users[socket.id] = name;
-      socket.broadcast.emit('usrJoin', Object.values(users), name);
       socket.emit('init-users', Object.values(users));
+      socket.broadcast.emit('usrJoin', Object.values(users), name);
    });
 
    socket.on('message', (data) => {
